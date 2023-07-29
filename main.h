a01ba18864341871afbed16fe4929df1cf1f87a7
Fagr Elaskary <48035751+fagrahmed12@users.noreply.github.com>
2023-07-29 06:05:03 +0300

    Update main.h

--- main.h
+++ main.h
@@ -5,22 +5,23 @@
 #include <unistd.h>
 
 #define UNUSED(x) (void)(x)
-#define BUFF_SIZE 1024
+#define BUFF_SIZE 10
+ 
 
-/* FLAGS */
+
+     /* flag */
 #define F_MINUS 1
 #define F_PLUS 2
 #define F_ZERO 4
 #define F_HASH 8
 #define F_SPACE 16
 
-/* SIZES */
+      /* size */
 #define S_LONG 2
 #define S_SHORT 1
 
 /**
  * struct fmt - Struct op
- *
  * @fmt: The format.
  * @fn: The function associated.
  */
@@ -43,7 +44,7 @@ int _printf(const char *format, ...);
 int handle_print(const char *fmt, int *i,
 va_list list, char buffer[], int flags, int width, int precision, int size);
 
-/****************** FUNCTIONS ******************/
+
 
 /* Funtions to print chars and strings */
 int print_char(va_list types, char buffer[],
@@ -106,7 +107,7 @@ int write_unsgnd(int is_negative, int ind,
 char buffer[],
 	int flags, int width, int precision, int size);
 
-/****************** UTILS ******************/
+
 int is_printable(char);
 int append_hexa_code(char, char[], int);
 int is_digit(char);
